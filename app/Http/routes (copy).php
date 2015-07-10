436e72055c6b7a856bc626d3ad4181cb4f6ae7ed
ramesh <ramesh.gurung@f1soft.com>
2015-07-10 11:33:22 +0545

    cleaning route file

--- app/Http/routes (copy).php
+++ app/Http/routes (copy).php
@@ -30,7 +30,34 @@ Route::get('env',function(){
 });
 
 
+/*
+Route::get('category/{id}',function($id){
+	return "get post of category $id";
+});
+//named route
+Route::get('category/post/date/images/top3',['as'=>'top3',function(){
+	return "top 3 images for any date of post under category";
+}]);
+Route::get('/front/category',function(){
+	$data['categories']=['uncategoried','beauty','fitness'];
+	$data['subView']='front/templates/category';
+	return View::make('front/layout',$data);
+});
+*/
 
+Route::get('post/images/{title?}',function($title='cover_pic'){
+	return "get image/images of post $title";
+});
+
+
+//regular expression
+Route::get('article/{id}',function($id){
+	return "article id: $id number [0-9]+'";
+})->where('id','[0-9]+');
+
+Route::get('article/{slug}',function($slug){
+	return "article slug: $slug alphabet [A-Za-z]+";
+})->where('slug','[A-Za-z]+');
 //Global Constraints better approach refer file
 // /var/www/html/rax/laravel_5.1/app/Providers/RouteServiceProvider.php
 Route::get('group/{id}',function($id){
@@ -43,7 +70,7 @@ Route::get('group/{slug}',function($slug){
 
 //group route with middleware
 Route::group(['middleware' => 'auth','as' => 'admin::'], function () {
-	Route::get('admin/dashboard', ['as' => 'dashboard', function () {
+	Route::get('dashboard', ['as' => 'dashboard', function () {
         // Route named "admin::dashboard"
 	}]);
 });
@@ -61,6 +88,40 @@ Route::group(['as'=>'donee::'],function(){
 Route::get('group/{name?}', function ($name = 'John') {
 	return $name;
 });
+Route::get('user/profile', [
+	'as' => 'profile', 'uses' => 'Profile\UserController@showProfile'
+	]);
+
+
+Route::get('frontend/',function(){
+	
+	
+	// return $url = route('top3');
+	// return $url = route('donee::dashboard');
+	return $url = route('donee::campaign');
+
+	// return $url = route('profile', ['id' => 1]);
+	
+	return View::make('front/main');
+});
+
+
+//prefix route
+Route::group(['prefix' => 'admin'], function () {
+	Route::get('users', function ()    {
+		echo "Matches The /admin/users URL";
+	});
+});
+
+Route::get('/jpt',function(){
+	return abort(404);
+});
+
+//custom middleware
+Route::get('/senior_citizen',['middleware' => 'age',function(){
+	return "only for senior citizen";
+}]);
+
 
 
 //some front end routes
@@ -91,9 +152,32 @@ Route::post('/front/login',function(){
 	return "$username : $password";
 });
 
+Route::get('/front/secret',array(
+	'before'=>'auth.basic',
+	function()
+	{
+		return "inside  secret";
+	}
+	));
+
 
 // some blades
 Route::group(['as'=>'blade::'],function(){
+	
+	// Route::get('blade',['as'=>'blade_layout',function(){
+	// 	// return "blade_layout";
+	// 	return view('layouts/blade_layout');
+	// }]);
+	// Route::get('blade/header',['as'=>'blade_header',function(){
+	// 	return "blade_header";		
+	// }]);
+	// Route::get('blade/footer',['as'=>'blade_footer',function(){
+	// 	return "blade_footer";		
+	// }]);
+	// Route::get('blade/main_content',['as'=>'blade_main_content',function(){
+	// 	return "blade_main_content";		
+	// }]);
+
 	//home, about
 	Route::get('home',['as'=>'home',function(){
 		return view('layouts/home');
@@ -111,13 +195,17 @@ Route::group(['prefix' => 'dashboard','as' => 'dashboard::'], function () {
 	}]);
 
 	//post management
+
 	// Route::get('users/banned', ['as' => 'admin.access.users.banned', 'uses' => 'UserController@banned']);
+	
 	Route::get('post', ['as' => 'post', 'uses'=>'PostController@index']);
+
 	// Route::get('post/{slug}/edit',['as'=>'post_edit', function(App\Post $post)
 	// {
 	// 	dd($post);
 	// 	// return print_r($post);
 	// }]);
+
 	Route::get('post/add', ['as' => 'post_create', 'uses'=>'PostController@create']);
 	Route::post('post/add', ['as' => 'post_store', 'uses'=>'PostController@store']);
 	Route::get('post/{slug}', ['as' => 'post_show', 'uses'=>'PostController@show']);
@@ -128,6 +216,14 @@ Route::group(['prefix' => 'dashboard','as' => 'dashboard::'], function () {
 
 	Route::resource('category', 'CategoryController');
 	
+	// Route::get('category', ['as' => 'category', 'uses'=>'CategoryController@index']);
+	// Route::get('category/add', ['as' => 'category_create', 'uses'=>'CategoryController@create']);
+	// Route::post('category/add', ['as' => 'category_store', 'uses'=>'CategoryController@store']);
+	// Route::get('category/{slug}/edit', ['as' => 'category_edit', 'uses'=>'CategoryController@edit']);
+	// Route::post('category/{slug}/edit', ['as' => 'category_update', 'uses'=>'CategoryController@update']);
+	// Route::get('category/{slug}/delete', ['as' => 'category_destroy', 'uses'=>'CategoryController@destroy']);
+
+
 });
 
 
